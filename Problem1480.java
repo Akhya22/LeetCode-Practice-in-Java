2b3e471b34345e4fda11f3c3e5e911e855435ec0
Akhya Tiwari <akhyatiwari22@gmail.com>
2026-01-24 20:38:18 +0530

    LeetCode Problem1480 Solution

---
+++ Problem1480.java
@@ -0,0 +1,18 @@
+//Take another array to store running sum
+//Initialize first element
+//Run the loop, add previous sum to the current element
+//brr[i]=brr[i-1]+arr[i]
+
+class Solution {
+    public int[] runningSum(int[] arr) {
+        int[] brr = new int[arr.length];
+        brr[0]=arr[0];
+        for(int i=1;i<brr.length;i++){
+            brr[i]=brr[i-1]+arr[i];
+        }
+        return brr;
+    }
+}
+
+//Time complexity: O(n)
+//Space complexity: O(n)
