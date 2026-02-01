3df9461623bd334a42a5e147f1f69c0d20c19f70
Akhya Tiwari <akhyatiwari22@gmail.com>
2026-02-02 00:48:11 +0530

    LeetCode Problem977 Solution

---
+++ Problem977.java
@@ -0,0 +1,20 @@
+//977. Squared of a Sorted Array
+
+//Sqaure the elements and store in their respective positions while looping
+//Sort the array and return
+
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            nums[i]=nums[i]*nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
+
+//Time Complexity: O(n*logn)
+//Space Complexity: O(1)     (ignoring internal sort stack space)
+
+//Note: Can be optimized further
