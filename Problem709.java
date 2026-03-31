1774f7e22b3d9b8a5b950cf824552026b750fd7c
Akhya Tiwari <akhyatiwari22@gmail.com>
2026-03-31 19:53:51 +0530

    LeetCode Problem709 Solution

---
+++ Problem709.java
@@ -0,0 +1,11 @@
+//709. To Lower Case
+
+class Solution {
+    public String toLowerCase(String s) {
+        s=s.toLowerCase();
+        return s;
+    }
+}
+
+//Time Complexity: O(n)
+//Space Complexity: O(n)
